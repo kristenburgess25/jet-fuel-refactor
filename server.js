635af929ec87d8ae55ea43632dc6e39890d47098
gness1804 <graham.nessler@gmail.com>
2017-01-23 17:50:23 -0700

    Got index html rendering

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const http = require("http");
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const path = require('path');
+let folders = {};
 
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (request, response) => {
+    response.sendFile(path.join(__dirname + '/lib/index.html'));
+});
 
 app.listen(app.get('port'), () => {
   console.log('The HTTP server is listening at Port 3000.');
@@ -10,6 +16,7 @@ app.listen(app.get('port'), () => {
 
 app.get('/', (request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/plain' });
+  // response.sendFile(path.join(__dirname + 'lib/index.html'));
   response.write('Hello World');
   response.end();
 });
