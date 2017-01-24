b8c0c1969204aa0bdeb47f3325ecbeaf48a6c3ca
gness1804 <graham.nessler@gmail.com>
2017-01-23 18:56:33 -0700

    Finally got scripts and styles hooked up

--- server.js
+++ server.js
@@ -4,19 +4,25 @@ const app = express();
 const path = require('path');
 let folders = {};
 
+app.use(express.static('public'));
+
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', (request, response) => {
-    response.sendFile(path.join(__dirname + '/lib/index.html'));
+  response.sendFile(path.join(__dirname, 'public/index.html'));
 });
+
+// app.get('/', (request, response) => {
+//   response.sendFile(path.join(__dirname, 'public/index.js'));
+// });
 
 app.listen(app.get('port'), () => {
   console.log('The HTTP server is listening at Port 3000.');
 });
 
-app.get('/', (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  // response.sendFile(path.join(__dirname + 'lib/index.html'));
-  response.write('Hello World');
-  response.end();
-});
+// app.get('/', (request, response) => {
+//   response.writeHead(200, { 'Content-Type': 'text/plain' });
+//   // response.sendFile(path.join(__dirname + 'lib/index.html'));
+//   response.write('Hello World');
+//   response.end();
+// });
