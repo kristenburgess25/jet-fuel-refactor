b957bf9fcbfc704548a0d134ab8107ba2e6f4fd0
Kristen Burgess <kristen.burgess@doane.edu>
2017-01-25 12:03:02 -0700

    Grab URLs from data object, not dynamic

--- public/index.js
+++ public/index.js
@@ -11,17 +11,22 @@ const makeAPICall = () => {
     if (hitAPI.readyState === XMLHttpRequest.DONE) {
       if (hitAPI.status === 200) {
         document.querySelector('#bookmark-folder-input').innerHTML = '';
+
         let defaultOption = document.createElement('OPTION');
         let text = document.createTextNode('Folder Name for this Bookmark');
+
         defaultOption.appendChild(text);
         document.querySelector('#bookmark-folder-input').appendChild(defaultOption);
         for (let prop in JSON.parse(hitAPI.responseText)) {
+          let longURL = JSON.parse(hitAPI.responseText).sports.urls
+          console.log('test', longURL);
           let opt = document.createElement('OPTION');
           opt.value = prop;
           let text1 = document.createTextNode(prop);
           opt.appendChild(text1);
           document.querySelector('#bookmark-folder-input').appendChild(opt);
-          document.querySelector('#main-folder-display').appendChild(opt);
+          document.querySelector('#folders-list').appendChild(opt);
+
         }
         console.log('The server response', JSON.parse(hitAPI.responseText));
       } else {
