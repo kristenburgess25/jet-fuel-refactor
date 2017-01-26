1a017d7b872e19297291267f9a8085ec5dee5dba
Kristen Burgess <kristen.burgess@doane.edu>
2017-01-25 18:18:52 -0700

    Pass parentFolder and id to goToRealURL function

--- public/index.js
+++ public/index.js
@@ -46,11 +46,14 @@ const fetchDisplay = () => {
             let urls = response[key].urls;
               urls.map((link) => {
                 let longURL = link.longURL;
+                let parentFolder = link.parentFolder;
+                let id = link.bookmarkId;
+                console.log(link);
                 newArr.push(`
                 <div
                 id="${link.bookmarkId}"
                 >
-                <p onClick="goToRealURL('${longURL}')">${link.shortURL}<p>
+                <p onClick="goToRealURL('${longURL}, ${parentFolder}, ${id}')">${link.shortURL}<p>
                 <p>${link.dateAddedHumanReadable}</p>
                 </div>
                 `)
@@ -70,9 +73,11 @@ const fetchDisplay = () => {
   }
 }
 
-const goToRealURL = (url) => {
-  console.log(url);
-  //post request to register click for the bookmark
+const goToRealURL = (url, folder, id) => {
+  // console.log(url, folder, id);
+
+  //put request to register click for the bookmark
+  axios.put("/bookmarks/sports/1", null)
   //redirect to longURL
 }
 
